c82f91946968e54e823bef59203ddeb632320ce4
Jeff Allen <jeffallen13@gmail.com>
2026-03-01 11:30:47 -0500

    Implemented metric drop down

--- app.py
+++ app.py
@@ -6,25 +6,29 @@ df = pd.read_csv('data/ofac_panel.csv')
 
 app = Dash()
 
+metrics = ['levels', 'additions', 'removals', 'change']
+
 app.layout = [
     html.H1(children='OFAC Time Series Explorer', style={'textAlign':'center'}),
-    dcc.Dropdown(df.Country.unique(), 'China', id='dropdown-selection'),
+    dcc.Dropdown(sorted(df.Country.unique()), 'China', id='dropdown-country'),
+    dcc.Dropdown(metrics, 'levels', id='dropdown-metric'),
     dcc.Graph(id='graph-content')
 ]
 
 @callback(
     Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value'),
+    Input('dropdown-country', 'value'),
+    Input('dropdown-metric', 'value'),
 )
-def update_graph(selected_country):
+def update_graph(selected_country, selected_metric):
     dff = df[df.Country==selected_country]
-    fig = px.line(dff, x='Date', y='levels')
+    fig = px.line(dff, x='Date', y=selected_metric)
     fig.update_layout(
         title={
-            'text': f'Number of entities on OFAC lists ({selected_country})',
+            'text': f'{selected_metric.capitalize()} of entities on OFAC lists ({selected_country})',
             'x': 0.5,
             'xanchor': 'center',
-            'yanchor': 'top' 
+            'yanchor': 'top'
         },
         xaxis_title='Monthly',
         yaxis_title=''
